3a7afc442197a7bffe6d12083b41687ffb7c6c6b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-06-16 15:02:46 +0200

    devpi config and service, needs upgrade path

--- quickstart-ansible.bash
+++ quickstart-ansible.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 ([ -d .env ] || python3 -m venv .env) &&
-.env/bin/pip install -Uq pip setuptools wheel &&
-.env/bin/pip install -Uq ansible "pywinrm>=0.1.1" click
+.env/bin/pip install -Uq pip setuptools setuptools_scm wheel &&
+.env/bin/pip install -Uq ansible~=2.7.0 mitogen "pywinrm>=0.1.1" click
